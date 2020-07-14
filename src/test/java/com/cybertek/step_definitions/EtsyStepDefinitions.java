4b9ff726aeb621275e7326f160a22b1ffba3c56a
gurhank <gurhankocyigit@gmail.com>
2020-07-14 11:19:36 -0500

    cucumber day3

--- src/test/java/com/cybertek/step_definitions/EtsyStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/EtsyStepDefinitions.java
@@ -1,5 +1,6 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.EtsySearchPage;
 import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -8,6 +9,8 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 
 public class EtsyStepDefinitions {
+
+    EtsySearchPage etsySearchPage = new EtsySearchPage();
 
     @Given("User is on the Etsy home page")
     public void user_is_on_the_etsy_home_page() {
@@ -28,18 +31,23 @@ public class EtsyStepDefinitions {
     }
 
 
+
     @When("User searches {string} in the search box")
     public void userSearchesInTheSearchBox(String searchValue) {
-
-
-
+        etsySearchPage.searchBox.sendKeys(searchValue);
     }
 
     @And("User clicks to search button")
     public void userClicksToSearchButton() {
+        etsySearchPage.searchButton.click();
     }
 
     @Then("User should see {string} in the Etsy title")
-    public void userShouldSeeInTheEtsyTitle(String arg0) {
+    public void userShouldSeeInTheEtsyTitle(String expectedInTitle) {
+
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
     }
 }
