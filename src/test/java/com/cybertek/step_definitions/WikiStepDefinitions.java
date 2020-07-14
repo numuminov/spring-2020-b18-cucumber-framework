85ddc990e459eedbe999a7f16513d004b6f684cf
gurhank <gurhankocyigit@gmail.com>
2020-07-14 11:52:42 -0500

    cucumber day3

--- src/test/java/com/cybertek/step_definitions/WikiStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/WikiStepDefinitions.java
@@ -1,11 +1,16 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.WikiSearchPage;
 import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.Keys;
 
 public class WikiStepDefinitions {
+
+    WikiSearchPage wikiSearchPage = new WikiSearchPage();
 
     @Given("User is on the Wikipedia home page")
     public void user_is_on_the_wikipedia_home_page() {
@@ -18,11 +23,15 @@ public class WikiStepDefinitions {
     @When("User searches {string} in the wiki search page")
     public void user_searches_in_the_wiki_search_page(String string) {
 
+        wikiSearchPage.searchBox.sendKeys(string + Keys.ENTER);
 
     }
-    @Then("User should see {string} in the wiki title")
-    public void user_should_see_in_the_wiki_title(String string) {
 
+    @Then("User should see {string} in the wiki title")
+    public void user_should_see_in_the_wiki_title(String expectedInTitle) {
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
 
     }
 }
