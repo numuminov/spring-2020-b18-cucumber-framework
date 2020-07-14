9c3022d9f65600f9964f0fbb608e6ca15bf1d8d7
gurhank <gurhankocyigit@gmail.com>
2020-07-14 14:41:30 -0500

    cucumber day3

--- src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
@@ -64,8 +64,6 @@ public class GoogleStepDefinitions {
     @When("User searches {string}")
     public void userSearches(String searchValue) {
 
-
-
         //sending value into search box using page object
         googleSearchPage.searchBox.sendKeys(searchValue + Keys.ENTER);
 
