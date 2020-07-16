5e4d8a2ffa4e6c25312a551ecbb0ab6821f4a70b
gurhank <gurhankocyigit@gmail.com>
2020-07-16 10:51:20 -0500

    cucumber day4

--- src/test/java/com/cybertek/step_definitions/Hooks.java
+++ src/test/java/com/cybertek/step_definitions/Hooks.java
@@ -1,6 +1,9 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.utilities.Driver;
 import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
@@ -20,10 +23,18 @@ public class Hooks {
     @After(order = 1)
     public void tearDownScenario(Scenario scenario){
         //System.out.println("-----> After annotation: Closing browser");
+        //System.out.println("scenario.getName() = " + scenario.getName());
+        //System.out.println("scenario.getSourceTagNames() = " + scenario.getSourceTagNames());
+        //System.out.println("scenario.isFailed() = " + scenario.isFailed());
 
-        System.out.println("scenario.getName() = " + scenario.getName());
-        System.out.println("scenario.getSourceTagNames() = " + scenario.getSourceTagNames());
-        System.out.println("scenario.isFailed() = " + scenario.isFailed());
+
+        //#1 we need to take a screen shot using SELENIUM -->
+        // getScreenshotAs: to be able to use this method we have to cast our driver type to TakesScreenshot
+        ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+        //#2 we are going to attach it into our report
+
+
+
 
 
     }
