4bb4150805c3b9b1ab6301d1833919e8e1e34440
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-07-21 17:02:30 -0400

    Added selenium grid config

--- src/test/java/com/cybertek/utilities/Driver.java
+++ src/test/java/com/cybertek/utilities/Driver.java
@@ -1,11 +1,15 @@
 package com.cybertek.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.net.MalformedURLException;
@@ -43,15 +47,30 @@ public class Driver {
                     break;
                 case "chrome-remote":
                     try {
-                        ChromeOptions chromeOptions = new ChromeOptions();
-                        URL url = new URL("http://100.24.235.239:4444/wd/hub");
-                        driver = new RemoteWebDriver(url, chromeOptions);
+//                        same thing as ChromeOptions
+//                        To request Selenium Grid to run tests on Chrome
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setCapability(CapabilityType.BROWSER_NAME, BrowserType.CHROME);
+                        desiredCapabilities.setPlatform(Platform.LINUX);
+//                        ChromeOptions chromeOptions = new ChromeOptions();
+                        URL url = new URL("http://3.235.145.39:4444/wd/hub");
+                        driver = new RemoteWebDriver(url, desiredCapabilities);
                     } catch (MalformedURLException e) {
                         e.printStackTrace();
                     }
                     break;
+                case "firefox-remote":
+                    try {
+                        //to request grid to run tests on firefox
+                        FirefoxOptions firefoxOptions = new FirefoxOptions();
+                        URL url = new URL("http://3.235.145.39:4444/wd/hub");
+                        driver = new RemoteWebDriver(url, firefoxOptions);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+
                 default:
-                    throw new RuntimeException("Wrong browser name :: "+browser);
+                    throw new RuntimeException("Wrong browser name: "+browser);
             }
         }
 
